e0cd95cbd25ebe9b94c18b6c5ad58c15c8f3b84a
Jacob Lang <jacoblang72@comcast.net>
2023-10-09 13:36:55 -0400

    package.json added commands and created schema in migration

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+
+
+exports.up = async function (knex) {
+  await knex.schema.createTable("cars",table=> {
+    table.increment("car_id");
+    table.string("car_vin").notNullable().unique();
+    table.string("car_make").notNullable();
+    table.string("car_model").notNullable();
+    table.integer("car_mileage").notNullable();
+    table.string("car_title");
+    table.string("car_transmission"); 
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("cars")
 };
